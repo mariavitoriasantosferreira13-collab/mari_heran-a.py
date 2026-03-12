195971be32dacb427894e6d47b92ad0e65da3d8f
Mariaa.v <mariavitoriasantosferreira13@gmail.com>
2026-03-12 17:26:08 -0300

    Add files via upload

---
+++ atividade13.py
@@ -0,0 +1,13 @@
+class Veiculo:
+    def __init__(self,marca):
+        self.marca = marca
+
+    def mover(self):
+        print("O carro está se movendo")
+
+class Carro(Veiculo):
+    def mover(self):
+        print(f"O Carro da {self.marca} está se movendo")
+
+car1 = Carro("Renault")
+car1.mover()
